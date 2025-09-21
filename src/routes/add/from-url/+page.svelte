140198cb9dd66700176b861b628eded4a80ed64e
Brennan <bafowler98@gmail.com>
2025-09-21 14:44:04 -0300

    store form value in url var

--- src/routes/add/from-url/+page.svelte
+++ src/routes/add/from-url/+page.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
+	import { Button } from '$lib/components/ui/button/index.js';
 	import { Input } from '$lib/components/ui/input/index.js';
 	import { Label } from '$lib/components/ui/label/index.js';
+
+	let url = $state('');
+
+	function handleGo() {
+		console.log(url);
+	}
 </script>
 
 <div>
@@ -9,5 +16,6 @@
 
 <div class="flex w-full max-w-sm flex-col gap-1.5">
 	<Label for="url" class="semi-bold">Recipe URL</Label>
-	<Input type="url" id="url" placeholder="https://" />
+	<Input type="url" id="url" placeholder="https://" bind:value={url} />
+	<Button variant="outline" disabled={!url} onclick={handleGo}>Go</Button>
 </div>
