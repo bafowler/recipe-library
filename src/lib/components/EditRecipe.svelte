b5e430e8f7365cc5d66f432cc281ba346e16c2c5
Brennan <bafowler98@gmail.com>
2025-10-05 21:52:12 -0300

    make instructions also editable

--- src/lib/components/EditRecipe.svelte
+++ src/lib/components/EditRecipe.svelte
@@ -5,6 +5,7 @@
 	import TrashIcon from '@lucide/svelte/icons/trash';
 	let { recipe } = $props();
 	let ingredients = $state(recipe.ingredients);
+	let instructions = $state(recipe.instructions);
 
 	function handleAddIngredient() {
 		ingredients.push('');
@@ -13,19 +14,27 @@
 	function handleRemoveIngredient(ingredient: string) {
 		ingredients = ingredients.filter((i: string) => i !== ingredient);
 	}
+
+	function handleAddInstruction() {
+		instructions.push('');
+	}
+
+	function handleRemoveInstruction(index: number) {
+		instructions = instructions.filter((_: string, i: number) => i !== index);
+	}
 </script>
 
-<div class="flex flex-col items-center">
+<div class="flex w-full flex-col items-center">
 	<h2>{recipe.name}</h2>
 
-	<div class="flex flex-col gap-4 pl-8">
+	<div class="flex w-full flex-col gap-4 pl-8">
 		<div>
 			<h4 class="mb-2">Ingredients</h4>
 			<div class="flex flex-col gap-4">
 				<ul class="flex flex-col gap-4 pl-8">
 					{#each ingredients as ingredient}
 						<Field.Field orientation="horizontal">
-							<Input id={ingredient} value={ingredient} />
+							<Input id={ingredient} value={ingredient} class="flex-grow-1" />
 							<Button
 								variant="secondary"
 								size="icon"
@@ -44,15 +53,26 @@
 		</div>
 
 		<div>
-			<h4>Instructions</h4>
+			<h4 class="mb-2">Instructions</h4>
 			<div class="flex flex-col gap-4">
-				<ol class="pl-8">
-					{#each recipe.instructions as instruction, index}
-						<li><span class="font-semibold">{index + 1}.</span> {instruction}</li>
+				<ol class="flex flex-col gap-4 pl-8">
+					{#each instructions as instruction, index}
+						<Field.Field orientation="horizontal">
+							<span class="font-semibold">{index + 1}.</span>
+							<Input id={instruction} value={instruction} />
+							<Button
+								variant="secondary"
+								size="icon"
+								class="size-8"
+								onclick={() => handleRemoveInstruction(index)}
+							>
+								<TrashIcon />
+							</Button>
+						</Field.Field>
 					{/each}
 				</ol>
 				<div class="pl-8">
-					<Button variant="outline">Add Instruction</Button>
+					<Button variant="outline" onclick={handleAddInstruction}>Add Instruction</Button>
 				</div>
 			</div>
 		</div>
